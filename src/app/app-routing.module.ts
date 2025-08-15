6894b69e5bbb2ed8c602aef60608684f11a63c3c
Abdourahmane <daffabdourahmane50@gmail.com>
2025-08-15 01:01:20 +0000

    login modfifier1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  // { path: 'login', component: LoginComponent }, // Votre page de connexion
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
