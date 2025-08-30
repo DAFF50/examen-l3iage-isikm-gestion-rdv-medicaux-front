a454b7923f9e02f53b9fc903f8732dc2d1157d12
Abdourahmane <daffabdourahmane50@gmail.com>
2025-08-30 01:57:51 +0000

    developpement front office patient part3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,26 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {SignupComponent} from './components/signup/signup.component';
 import {SidebarComponent} from "./components/patient/sidebar/sidebar.component";
+import {DashboardComponent} from "./components/patient/dashboard/dashboard.component";
+import {ChatAiComponent} from "./components/patient/chat-ai/chat-ai.component";
+import {NotificationsComponent} from "./components/patient/notifications/notifications.component";
+import {MyAppointmentsComponent} from "./components/patient/my-appointments/my-appointments.component";
+import {TakeAppointmentComponent} from "./components/patient/take-appointment/take-appointment.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'patient', component: SidebarComponent},
+  {
+    path: 'patient', component: SidebarComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', component: DashboardComponent },
+      { path: 'chat_ai', component: ChatAiComponent },
+      { path: 'notification', component: NotificationsComponent },
+      { path: 'my-appointments', component: MyAppointmentsComponent },
+      { path: 'take-appointment', component: TakeAppointmentComponent },
+    ]
+  },
   {path: '', component: LoginComponent},
   {path: '**', redirectTo: ''}
 ]
--- src/app/components/patient/notifications/notifications.component.ts
+++ src/app/components/patient/notifications/notifications.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NotificationsComponent {
 
+  // Gestion des filtres
+  activeFilter: string = 'toutes';
+
+  setFilter(filter: string) {
+    this.activeFilter = filter;
+  }
 }
